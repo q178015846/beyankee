2fb315bd80e1d60ceb30722e1c6f55c5ecc882ed
Rock <178015846@qq.com>
2015-10-21 14:13:08 +0800

    test

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'url' => 'http://www.beyankee.com/beyankee/public',
+	'url' => 'http://www.beyankee.com',
 
 	/*
 	|--------------------------------------------------------------------------
